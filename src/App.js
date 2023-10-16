a74192c456feeb0a40531515935df1f0cf98ba85
Austin <aengelbrecht>
2023-10-16 11:23:37 -0500

    Use props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-const Pet = () => {
+const Pet = (props) => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Daisy"),
-    React.createElement("h2", {}, "Dog"),
-    React.createElement("h2", {}, "German Shepherd"),
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h2", {}, props.breed),
   ]);
 };
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet),
+    React.createElement(Pet, {
+      name: "Daisy",
+      animal: "Dog",
+      breed: "German Shepherd",
+    }),
   ]);
 };
 
