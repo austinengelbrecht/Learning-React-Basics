94f328cc4f2b5bc93c27e28307bf326281ae19ad
Austin <aengelbrecht>
2023-10-16 15:10:24 -0500

    Search Params Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 // import React from "react";
 import { createRoot } from "react-dom";
-import Pet from "./Pet";
-
-// const Pet = (props) => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, props.name),
-//     React.createElement("h2", {}, props.animal),
-//     React.createElement("h2", {}, props.breed),
-//   ]);
-// };
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Daisy" animal="dog" breed="GSD" />
-      <Pet name="Abby" animal="dog" breed="Dorkie" />
+      <SearchParams />
     </div>
   );
 };
