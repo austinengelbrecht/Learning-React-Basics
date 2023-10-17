7e03285335bab36d06460d933cf895f74316d537
Austin <aengelbrecht>
2023-10-17 13:14:10 -0500

    Add basic router

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,18 @@ import { createRoot } from "react-dom/client";
 // import Pet from "./Pet";
 import SearchParams from "./SearchParams";
 
+//Routing
+import { BrowserRouter } from "react-router-dom";
+import Details from "./Details";
+
 const App = () => {
   return (
-    <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <BrowserRouter>
+      <div>
+        <h1>Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </BrowserRouter>
   );
 };
 
