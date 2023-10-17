20d48ef2449f1a93bf95b8c6355e895b83aa724c
Austin <aengelbrecht>
2023-10-17 14:20:33 -0500

    Add and implement react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,32 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import SearchParams from "./SearchParams";
 import Details from "./Details";
 
+const QueryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt ME</Link>
-      </header>
+      <QueryClientProvider client={QueryClient}>
+        <header>
+          <Link to="/">Adopt ME</Link>
+        </header>
 
-      <Routes>
-        <Route path="/details/:id" element={<Details />} />
-        <Route path="/" element={<SearchParams />} />
-      </Routes>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
+        </Routes>
+      </QueryClientProvider>
     </BrowserRouter>
   );
 };
